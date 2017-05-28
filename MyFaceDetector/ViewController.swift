c4ebb93c04a25d72bc2232616bb45c97159446bf
Pedro Velmovitsky <pedrovelmo@gmail.com>
2017-05-28 20:24:33 -0300

    right eye positioned

---
+++ MyFaceDetector/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  MyFaceDetector
+//
+//  Created by Pedro Velmovitsky on 28/05/17.
+//  Copyright © 2017 velmovitsky. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "image1")
+        print("View Size", self.view.frame.size)
+        print("Image View Size", self.imageView.frame.size)
+        print("Image Size", self.imageView.image?.size)
+        
+        detect()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func detect() {
+        
+        let personciImage = CIImage(cgImage: imageView.image!.cgImage!)
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector?.features(in: personciImage)
+       
+        
+        
+        let ciImageSize = personciImage.extent.size
+        var transform = CGAffineTransform(scaleX: 1, y: -1)
+        transform = transform.translatedBy(x: 0, y: -ciImageSize.height)
+        if let face = faces?.first as? CIFaceFeature {
+            print("found bounds are \(face.bounds)")
+            
+            
+            //var faceViewBounds = face.bounds
+            //let viewSize = imageView.bounds.size
+            
+            let scale = scaleImage(before: (self.imageView.image?.size)!, after: self.view.frame.size)
+            print("Scale = ", scale)
+ 
+            if face.hasSmile {
+                print("face is smiling ");
+            }
+            
+            if face.hasMouthPosition {
+                print("Mouth bounds are \(face.mouthPosition)")
+            }
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.rightEyePosition)")
+                let rightEye = CALayer()
+                let image = UIImage(named: "rightEye")
+                rightEye.contents = image?.cgImage
+                rightEye.contentsGravity = kCAGravityCenter
+                print("Right Eye height", image?.size.height)
+                
+                var rightEyePosition = face.rightEyePosition
+                rightEyePosition.x = rightEyePosition.x * scale
+                //rightEyePosition.y = rightEyePosition.y * scale
+                    
+               rightEyePosition.y = ((self.imageView.image?.size.height)! - rightEyePosition.y) * scale + (image?.size.height)!
+                    
+
+                rightEye.position = rightEyePosition
+                imageView.layer.addSublayer(rightEye)
+                print("rightEyePosition: ", rightEyePosition)
+             
+                }
+            } else {
+                print("No faces found")
+            }
+        }
+    
+    
+    func scaleImage(before: CGSize, after: CGSize) -> CGFloat {
+        
+        let height = after.height/before.height
+        let width = after.width/before.width
+        
+        return width < height ? width : height
+        
+    }
+
+
+}
+
